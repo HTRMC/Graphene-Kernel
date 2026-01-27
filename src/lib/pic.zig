b62d58fd1bff79899fadc0baac2988ee7d21e59b
HTRMC <htrmcox@outlook.com>
2026-01-27 12:30:35 +0100

    fix(pic): fix port constraint in PIC I/O functions

--- src/lib/pic.zig
+++ src/lib/pic.zig
@@ -22,7 +22,7 @@ fn outb(port: u16, value: u8) void {
     asm volatile ("outb %[value], %[port]"
         :
         : [value] "{al}" (value),
-          [port] "N{dx}" (port),
+          [port] "{dx}" (port),
     );
 }
 
@@ -30,7 +30,7 @@ fn outb(port: u16, value: u8) void {
 fn inb(port: u16) u8 {
     return asm volatile ("inb %[port], %[result]"
         : [result] "={al}" (-> u8),
-        : [port] "N{dx}" (port),
+        : [port] "{dx}" (port),
     );
 }
 
